732252b6ae50f81dbc3ee9fa121e6315b1edd512
Bogong Yang <cmdroot9908@gmail.com>
2021-03-26 00:42:48 -0400

    Init project and test connection with backend

---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'Hello',
+    data() {
+        return {
+            msg: '',
+        };
+    },
+    created() {
+        axios.get('http://127.0.0.1:5000/api/hello-world').then((res) => {
+            console.log(res);
+            this.msg = res.data.msg;
+        });
+    },
+};
+</script>
